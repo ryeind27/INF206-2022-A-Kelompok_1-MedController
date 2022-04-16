0fa54547301fe4df8d7ec97044a02c5d798a9c96
T.M. Roy Adrian <tmroyadrian098@gmail.com>
2022-04-16 22:06:40 +0700

    create signIn view & index style
    add image, create signIn view, dan menambahkan style kedalam tampilan view

---
+++ MedController/resources/views/signIn.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/index.css">
+    
+    <title>MedController/Login</title>
+  </head>
+  <body>
+    <!-- Login Form -->
+    <div class="container auth-container">
+        <div class="row">
+            <div class="col-md-4 login text-center">
+                <img class="mb-4" src="./images/avatar.png">
+                <form>
+                    <div class="mb-3">
+                        <input name="email" type="email" class="form-control" placeholder="Alamat Email">
+                    </div>
+                    <div class="mb-3">
+                        <input name="password" type="password" class="form-control" placeholder="Password">
+                    </div>
+                    <button class="btn btn-primary w-100 mb-4">MASUK</button>
+                </form>
+                <span>Belum punya akun? <a href="#">Daftar!</a></span>
+            </div>
+        </div>
+    </div>
+   
+      
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    
+  </body>
+</html>
