fcaf910eaa7c52fe36e0f24dc2e4d05fbbd84380
Nurfajriati <fajriati@mhs.unsyiah.ac.id>
2022-04-22 02:22:13 +0700

    create navbar
    membuat navbar di halaman utama

---
+++ medcontroller/resources/views/navbar.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>MedController Menu</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-light fixed-top" style="background-color: #79F1A4;">
+  <div class="container">
+  <!-- Brand -->
+  <a class="navbar-brand" href="#">MedController</a> 
+  <!-- Links -->
+</div>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Resep</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Jadwal</a>
+      </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Chat</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">NamaUser</a>
+    </li>
+  </ul>
+  </div>
+  <img src="profile.jpeg" alt="Logo" style="width:50px;" class="rounded-circle" href="#"> 
+<!-- </div>-->
+</nav>
+<br><br><br>
+
+      <!-- <div class="alert alert-success" role="alert">
+        Pemberitahuan
+      </div>
+      <div class = "pemberitahuan" >
+      <a class = "alert" href = "#">Pemberitahuan pasien</a>
+      </div> -->
+      <div class="alert alert-success alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <h4 class="alert-heading">Pemberitahuan</h4>
+        <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+        <hr>
+        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+      </div>
+</body>
+</html>
