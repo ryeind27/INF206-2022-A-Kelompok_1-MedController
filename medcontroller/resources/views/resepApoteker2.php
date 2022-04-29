c29262df11a29578141ae38af15fbe7007a813a8
yulkhains <yulia_kh@mhs.unsyiah.ac.id>
2022-04-29 13:09:26 +0700

    Tampilan awal resep apoteker

---
+++ medcontroller/resources/views/resepApoteker2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Resep apoteker</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="Free Website Template" name="keywords">
+        <meta content="Free Website Template" name="description">
+
+        <!-- Favicon -->
+        <link href="img/favicon.ico" rel="icon">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:300;400;600;700;800&display=swap" rel="stylesheet">
+
+        <!-- Font Awesome -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="alert alert-success" role="alert">
+            <h1 class="alert-heading"></h1>
+            <h6>Dokter mengirim sebuah resep baru <br /><br />
+                Berikut ini adalah detail tentang resep obatnya <a class="btn btn-link p-0" href="">Read More <i class="fa fa-angle-right"></i></a> 
+            </h6><br /><br />
+        </div>
+   	</form>
+    </body>
+</html>
