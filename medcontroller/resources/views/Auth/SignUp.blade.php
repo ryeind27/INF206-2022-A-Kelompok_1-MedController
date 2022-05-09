04523286df1392ecd937ea4238bc6456697f24f7
T.M. Roy Adrian <tmroyadrian098@gmail.com>
2022-05-09 11:29:36 +0700

    add templating
    modify beberapa file untuk menambahkan templating laravel

--- medcontroller/resources/views/Auth/SignUp.blade.php
+++ medcontroller/resources/views/Auth/SignUp.blade.php
@@ -1,24 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
-    <!-- CSS -->
-    <link rel="stylesheet" href="/styles/index.css">
-
-    <title>MedController/SignUp</title>
-  </head>
-  <body>
-    <!-- Register Form -->
+@extends('.app')
+<!-- Register Form -->
+@section('content')
     <div class="container auth-container">
         <div class="row">
             <div class="col-md-4 login text-center">
@@ -38,14 +20,8 @@
                     </div>
                     <button class="btn btn-primary w-100 mb-4">DAFTAR</button>
                 </form>
-                <span>Sudah punya akun? <a href="{{url('/')}}">Masuk!</a></span>
+                <span>Sudah punya akun? <a href="{{url('/')}}">Klik Disini!</a></span>
             </div>
         </div>
     </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-
-  </body>
-</html>
+@endsection
