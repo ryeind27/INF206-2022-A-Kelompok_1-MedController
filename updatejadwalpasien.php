959cffa6d599ad8d27852b0f6d765f24356d797d
RahmiSusanti <rahmi.ss@mhs.unsyiah.ac.id>
2022-05-20 06:59:42 -0700

    update jadwal pasien

--- updatejadwalpasien.php
+++ updatejadwalpasien.php
@@ -2,6 +2,13 @@
 <html lang="en">
 
 <head>
+    <title>Alarm Pasien</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
     <!-- Title Page-->
     <title>Alarm</title>
 
@@ -38,24 +45,11 @@
         <td>Paracetamol</td>
         <td>Setelah makan</td>
       </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
     </tbody>
   </table>
 </div>
 
 </body>
 </html>
+
+ 
