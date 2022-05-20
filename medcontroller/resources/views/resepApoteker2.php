f64b92e5b4ec64b0812602250d9bbcba1d63bc83
yulkhains <yulia_kh@mhs.unsyiah.ac.id>
2022-05-20 23:12:49 +0700

    update notifikasi

--- medcontroller/resources/views/resepApoteker2.php
+++ medcontroller/resources/views/resepApoteker2.php
@@ -1,31 +1,64 @@
+<?php
+    include 'koneksi.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Resep apoteker</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-        <meta content="Free Website Template" name="keywords">
-        <meta content="Free Website Template" name="description">
 
-        <!-- Favicon -->
-        <link href="img/favicon.ico" rel="icon">
+<head>
+    <!-- Title Page-->
+    <title>Notifikasi Pasien</title>
 
-        <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:300;400;600;700;800&display=swap" rel="stylesheet">
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
 
-        <!-- Font Awesome -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
 
-        <!-- Customized Bootstrap Stylesheet -->
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-        <div class="alert alert-success" role="alert">
-            <h1 class="alert-heading"></h1>
-            <h6>Dokter mengirim sebuah resep baru <br /><br />
-                Berikut ini adalah detail tentang resep obatnya <a class="btn btn-link p-0" href="">Read More <i class="fa fa-angle-right"></i></a> 
-            </h6><br /><br />
+    <!-- Main CSS-->
+    <link href="css/style.css" rel="stylesheet" media="all">
+</head>
+
+<body>
+    <div class="page-wrapper bg-gra-03 p-t-45 p-b-50">
+        <div class="wrapper wrapper--w960">
+                <div class="judul">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td>Dokter mengirim sebuah resep baru<br /><br />
+                                    <div>
+                                        <button class="btn btn--radius btn--black" type="submit">
+                                            <a href = "resepApoteker.php?"  style="color:white">Detail</a>
+                                        </button>
+                                        <button class="btn btn--radius btn--black" type="submit">
+                                            <a href = "" onclick="return confirm('Apakah ingin menghapus?')" style="color:white">Delete</a>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-   	</form>
-    </body>
+    </div>
+    
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+
+</body>
+
 </html>
+<!-- end document-->
